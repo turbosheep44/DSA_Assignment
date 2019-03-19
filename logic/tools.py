2e93c9feb648dbf3865299f6e9845811ffae477b
turbosheep44 <matthew.axisa4@gmai.com>
2019-03-19 11:34:09 +0100

    created and added random array

---
+++ logic/tools.py
@@ -0,0 +1,21 @@
+import random
+
+# creates a random array with the given length (256 if this is unspecified)
+# the array is populated with numbers in the range min - max, endpoints included 
+def randomArray(min = 0, max = 1024, length = 256):
+    # if the parameters were passed badly (i.e. max is smaller than min) then swap them
+    if max < min:
+        temp = min
+        min = max
+        max = temp
+    
+    # create the empty array
+    array = []
+
+    # iterate 'length' times; use abs to ensure safety if a negative length is passed
+    for x in range(abs(length)):
+        # each time add a new random integer to the array
+        array.append(random.randint(min, max))
+
+    # return the new array
+    return array
