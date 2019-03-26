a6aed4c28b7dfe8557f00a7358cdc8d459ccb1e1
turbosheep44 <matthew.axisa4@gmail.com>
2019-03-26 10:25:13 +0100

    finished GUI

---
+++ gui/question7.py
@@ -0,0 +1,41 @@
+
+
+import tkinter as tk
+from tkinter import ttk
+
+from logic.question7 import Node
+
+
+def addToTree(input, outputField, rootNode):
+    print(rootNode.value)
+    if rootNode.value is None:
+        rootNode.value = float(input)
+    else:
+        rootNode.addNode(float(input))
+    
+    outputField.delete(1.0, tk.END)
+    outputField.insert(tk.END, rootNode.getDisplayString())
+
+
+def reset(outputField, rootNode):
+    rootNode.value = None
+    rootNode.left = None
+    rootNode.right = None
+    outputField.delete(1.0, tk.END)
+
+
+def makeGUI(window):
+    rootNode = Node()
+    root = ttk.Frame(window)
+    label = tk.Label(root, text="Enter a number to find the it's square root:")
+    label.pack()
+    inputField = tk.Entry(root)
+    inputField.pack()
+    outputField = tk.Text(root, height=30, width=100)
+    button = tk.Button(root, text="Add node", command=lambda: addToTree(inputField.get(), outputField, rootNode))
+    button2 = tk.Button(root, text="Reset", command=lambda: reset(outputField, rootNode))
+    button.pack()
+    button2.pack()
+    outputField.pack()
+
+    return root
