77edca7662ef17c11428bdc3d0590ec69ea250ec
turbosheep44 <matthew.axisa4@gmail.com>
2019-03-26 16:32:58 +0100

    added q4

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from tkinter import ttk
 import gui.question8 as q8
 import gui.question7 as q7 
+import gui.question4 as q4
 import gui.question3 as q3
 import gui.question2 as q2
 import gui.question1 as q1
@@ -18,6 +19,7 @@ tab.pack()
 tabController.add(q1.makeGUI(window), text='Question 1')
 tabController.add(q2.makeGUI(window), text='Question 2')
 tabController.add(q3.makeGUI(window), text='Question 3')
+tabController.add(q4.makeGUI(window), text='Question 4')
 tabController.add(q7.makeGUI(window), text='Question 7')
 tabController.add(q8.makeGUI(window), text='Question 8')
 tabController.pack(expand=1, fill="both")
