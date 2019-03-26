5fe43c8d5bd0ef2d5311bd6becb34659b8a5c0f0
turbosheep44 <matthew.axisa4@gmai.com>
2019-03-26 12:14:29 +0100

    better outpu

--- gui/question3.py
+++ gui/question3.py
@@ -1,6 +1,5 @@
 
 
-
 import logic.question3 as three
 import logic.tools as tools
 
@@ -10,9 +9,11 @@ import timeit
 import tkinter as tk
 from tkinter import ttk
 
+
 def checkSorted(inputString, outputField):
 
-    array = [ float(x.strip(' ')) for x in inputString.strip('\n').strip(' ').split(",") ]
+    array = [float(x.strip(' '))
+             for x in inputString.strip('\n').strip(' ').split(",")]
     extremePoints = three.findExtremes(array)
 
     outputField.delete(1.0, tk.END)
@@ -21,30 +22,28 @@ def checkSorted(inputString, outputField):
     if len(extremePoints) == 0:
         outputField.insert(tk.END, "SORTED")
     else:
-         for p in extremePoints:
-                outputField.insert(tk.END, "extreme point at index :")
-                outputField.insert(tk.END, p)
-                outputField.insert(tk.END, "\n")
-    
-
+        for p in extremePoints:
+            newLine = "extreme point at index : " + \
+                str(p) + "  [" + str(array[p-1]) + ", " + \
+                str(array[p]) + ", " + str(array[p+1]) + "]" + "\n"
+            outputField.insert(tk.END, newLine)
 
 
 def makeGUI(window):
 
     root = ttk.Frame(window)
-    tk.Label(root, text="Enter a list of numbers, with each element separated by a comma:").pack()
-    inputField = tk.Text(root,height=5)
-    inputField.pack(expand=1,fill=tk.X)
+    tk.Label(
+        root, text="Enter a list of numbers, with each element separated by a comma:").pack()
+    inputField = tk.Text(root, height=5)
+    inputField.pack(expand=1, fill=tk.X)
 
-    outputField = tk.Text(root,height=3)
-    
+    outputField = tk.Text(root, height=3)
+
     button = tk.Button(root, text="Find extreme points",
                        command=lambda: checkSorted(inputField.get(1.0, tk.END), outputField))
     button.pack()
-    
+
     tk.Label(root, text="output:").pack()
     outputField.pack(expand=1, fill=tk.X)
-
-
 
     return root
