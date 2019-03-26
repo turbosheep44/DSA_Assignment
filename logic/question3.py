396fd8c60cb7a10f06928d1724bd50e53d98986e
turbosheep44 <matthew.axisa4@gmai.com>
2019-03-26 11:48:52 +0100

    refactored to run as a method call

--- logic/question3.py
+++ logic/question3.py
@@ -1,12 +1,19 @@
 
-a = [3, 7, 9, 23, 56, 76, 99, 333, 1243, 6565]
 
-sorted = True
 
-for x in range(1, len(a) - 1):
-    if not (a[x-1] < a[x] < a[x+1]):
-        print("Extreme point found ", a[x])
-        sorted = False
+def findExtremes(a):
+        extremePoints = []
 
-if sorted:
-    print("SORTED LIST")
+        for x in range(1, len(a) - 1):
+                if not (a[x-1] < a[x] < a[x+1]):
+                        extremePoints.append(x)
+
+        return extremePoints
+
+if __name__ == '__main__':
+        a = [3, 7, 9, 23, 56, 76, 99, 81, 1243, 6565] 
+        extremes = findExtremes(a)
+        for p in extremes:
+                print("extreme point at index :", p)
+        if len(extremes) == 0:
+                print("list is sorted")
