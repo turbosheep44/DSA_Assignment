e86f1d4dfbc60b66ad13f9b1bc2d7e4d7ee474b3
turbosheep44 <matthew.axisa4@gmail.com>
2019-03-26 17:56:09 +0100

    created GUI

---
+++ gui/question10.py
@@ -0,0 +1,48 @@
+import logic.question10 as ten
+import logic.tools as tools
+
+import random
+import timeit
+
+import tkinter as tk
+from tkinter import ttk
+
+
+def findPairs(largest, smallest, length, outputField):
+    array = tools.randomArray(smallest, largest, length)
+    
+    outputField.delete(1.0, tk.END)
+    outputField.insert(tk.END, array.__str__())
+    outputField.insert(tk.END, "\n\nThe largest number is:")
+
+    largest = ten.findLargest(array)
+    outputField.insert(tk.END, "\n\t" + str(largest))
+
+
+def makeGUI(window):
+
+    root = ttk.Frame(window)
+    
+    tk.Label(root, text="Parameters for random array:").pack(pady=10)
+    inputFields = tk.Frame(root, height = 50)
+    inputFields.pack(fill= tk.X, pady=10)
+    tk.Label(inputFields, text="maximum: ").pack(side=tk.LEFT)
+    maxNumber = tk.Entry(inputFields)
+    maxNumber.pack(side=tk.LEFT)
+    tk.Label(inputFields, text="minimum: ").pack(side=tk.LEFT)
+    minNumber = tk.Entry(inputFields)
+    minNumber.pack(side=tk.LEFT)
+    tk.Label(inputFields, text="length: ").pack(side=tk.LEFT)
+    length = tk.Entry(inputFields)
+    length.pack(side=tk.LEFT)
+
+    outputField = tk.Text(root)
+
+    button = tk.Button(root, text="Generate array and find largest",
+                       command=lambda: findPairs(int(maxNumber.get()), int(minNumber.get()) ,int(length.get()),  outputField))
+    button.pack(pady=10)
+
+    tk.Label(root, text="output:").pack()
+    outputField.pack(expand=1, fill=tk.X, pady=10)
+
+    return root
