00d8010ff92e0044ca13efb2ac0ccfc6aad4c9d3
turbosheep44 <matthew.axisa4@gmail.com>
2019-04-23 17:59:32 +0200

    simplified test driver

--- logic/question4.py
+++ logic/question4.py
@@ -41,6 +41,19 @@ def findPairs(l):
     #print(len(output))
     return output
 
+def naive(a):
+    output = []
+    for x in range(len(a)):
+        for y in range (x, len(a)):
+            prod = x * y
+            for i in range(len(a)):
+                for j in range (x, len(a)):
+                    prod2 = i  * j
+                    if prod == prod2 and x != i and y != j and a[x]!= a[y] and a[x] != a[i] and a[x] != a[j]:
+                        output.append((x,y,i,j,prod))
+    return output
+
+
 
 # test stub
 #array = tools.randomArray(1, 1024, 100)
@@ -48,5 +61,13 @@ def findPairs(l):
 #print(*main(array), sep="\n")
 if __name__ == '__main__':
     import timeit
-    array = tools.arrayN(100)
-    print(timeit.timeit(lambda: findPairs(array[:]), number=1))
+    array = [12,4,5,3,6,8,33,55,11,4,6,78,19,22]
+    print(findPairs(array))
+
+    for x in range (50, 101, 10):
+        array = tools.randomArray(min = 1, length=x)
+        print()
+        print(x, " elements in the random array:")
+        print("\tproposed solution:\t",timeit.timeit(lambda: findPairs(array[:]), number=10))
+        print("\tembedded loop approach:\t",timeit.timeit(lambda: naive(array[:]), number=10))
+        
